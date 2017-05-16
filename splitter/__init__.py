f1a47ef05ae3ac4eafcf5c33d21af435e31fe3af
Muresan Ionut <muresan.ionut95@gmail.com>
2017-05-16 18:56:27 +0300

    improvements

--- splitter/__init__.py
+++ splitter/__init__.py
@@ -35,23 +35,47 @@ def split_image(file_path):
 
         os.makedirs(path)
     
-    generate_labels_for_back_window(image, original_width, original_height, move, path, filename)
-    generate_labels_for_back_left_headlight(image, original_width, original_height, move, path, filename)
-
     classifiers = [
-        'back_car_window': {
-            'end_x': 2,
+        {   
+            'classifier': 'back_car_window',
+            'end_x': 1,
+            'end_y': 1/2,
+            'start_x': 0,
+            'start_y': 0,
+            'move': 40,
+            'reverse': False,
+            'new_width_image': 1/2,
+            'new_height_image': 1/4,
+            'filename': filename
+        },
+        {
+            'classifier': 'back_left_headlight',
+            'end_x': 1/2,
             'end_y': 1,
             'start_x': 0,
             'start_y': 0,
-            'move': 20,
+            'move': 40,
+            'reverse': False,
+            'new_width_image': 1/5,
+            'new_height_image': 1/4,
+            'filename': filename
+        },
+        {
+            'classifier': 'back_right_headlight',
+            'end_x': 1,
+            'end_y': 1,
+            'start_x': 1/2,
+            'start_y': 0,
+            'move': 40,
             'reverse': True,
-            'new_width_image': 4,
-            'new_height_image': 4
+            'new_width_image': 1/5,
+            'new_height_image': 1/4,
+            'filename': filename
         }
     ]
 
-    generate_labels_for_classifier(image, path, classifier)
+    for classifier in classifiers:
+        generate_labels_for_classifier(image, path, classifier)
 
     return path
 
@@ -59,68 +83,30 @@ def generate_labels_for_classifier(image, path, classifier):
     original_height = image.shape[0]
     original_width = image.shape[1]
 
-    new_width_image = int(original_width / classifier['new_width_image'])
-    new_height_image = int(original_height / classifier['new_height_image'])
+    new_width_image = int(original_width * classifier['new_width_image'])
+    new_height_image = int(original_height * classifier['new_height_image'])
 
-    end_x = int(original_width / classifier['end_x'])
-    end_y = int(original_height / classifier['end_y'])
-    
-    i = original_width / move
-    j = original_height / move
+    end_x = int(original_width * classifier['end_x'])
+    end_y = int(original_height * classifier['end_y'])
 
-def generate_labels_for_back_left_headlight(image, original_width, original_height, move, path, filename):
-    original_height = image.shape[0]
-    original_width = image.shape[1]
+    move = int(original_width / classifier['move']) 
 
-    new_width_image = int(original_width / 4)
-    new_height_image = int(original_height / 4)
+    i = int(original_height * classifier['start_y'])
+    j = int(original_width * classifier['start_x'])
 
-    end_x = int(original_width / 2)
-    end_y = original_height
-
-    i = 0
-    j = original_height / move
-
-    new_path = os.path.join(path, 'back_left_headlight')
+    new_path = os.path.join(path, classifier['classifier'])
 
     os.makedirs(new_path)
 
-    while i * move + new_height_image < end_y:
-        while j * move + new_width_image < end_x:
+    while i + new_height_image <= end_y:
+        while j + new_width_image <= end_x:
             with tf.Graph().as_default(), tf.Session() as sess:
-                flipped_image = sess.run(tf.image.crop_to_bounding_box(image, i * move, j * move , new_height_image, new_width_image))
-                new_filename = filename + "_" + str(i) + "_" + str(j) + '.jpg'
-                save_image(flipped_image, new_path, new_filename)
-            j = j + 1
-        i = i + 1
-        j = 0
-
-def generate_labels_for_back_window(image, original_width, original_height, move, path, filename):
-    original_height = image.shape[0]
-    original_width = image.shape[1]
-
-    new_height_image = int(original_height / 4)
-    new_width_image = int(original_width / 2)
-
-    end_x = original_width
-    end_y = int(original_height / 2)
-
-    i = 0
-    j = 0
-
-    new_path = os.path.join(path, 'back_car_window')
-
-    os.makedirs(new_path)
-
-    while i * move + new_height_image < end_y:
-        while j * move + new_width_image < end_x:
-            with tf.Graph().as_default(), tf.Session() as sess:
-                flipped_image = sess.run(tf.image.crop_to_bounding_box(image, i * move, j * move , new_height_image, new_width_image))
-                new_filename = filename + "_" + str(i) + "_" + str(j) + '.jpg'
-                save_image(flipped_image, new_path, new_filename)
-            j = j + 1
-        i = i + 1
-        j = 0
+                cropped_image = sess.run(tf.image.crop_to_bounding_box(image, i, j, new_height_image, new_width_image))
+                new_filename = classifier['filename'] + "_" + str(i) + "_" + str(j) + ".jpg"
+                save_image(cropped_image, new_path, new_filename)
+            j = j + move
+        i = i + move
+        j = int(original_width * classifier['start_x'])
 
 def save_image(image, path, filename):
     a = Image.fromarray(image, "RGB")
